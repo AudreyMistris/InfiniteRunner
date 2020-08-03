6447cc5f6cdb34b70bb3128964de94b144bc585f
AudreyMistris <57502743+AudreyMistris@users.noreply.github.com>
2020-08-03 17:37:26 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+import LoadingScene from './loading-scene.js';
+import GameScene from './game-scene.js';
+
+const CONFIG = {
+    type: Phaser.AUTO,
+    width: 549,
+    height: 360,
+    parent: 'jogo-runner',
+
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: {
+                y: 100
+            },
+            debug: false
+        }
+    },
+
+    scene: [LoadingScene, GameScene]
+};
+
+const GAME = new Phaser.Game(CONFIG);
